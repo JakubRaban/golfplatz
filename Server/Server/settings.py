5ed638dedcd7399add18949fe40c182e064788c8
Mateusz Kubicki <drevan657@gmail.com>
2020-04-08 22:07:35 +0200

    updated react packages

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'golfplatz'
+    'golfplatz',
     'rest_framework',
     'frontend',
 ]
