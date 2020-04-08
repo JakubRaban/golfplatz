33d240cb657dcd19778b0b67ae55514391c5d29c
Jakub <jakub.raban@gmail.com>
2020-04-08 19:02:11 +0200

    Added Paths

--- Server/golfplatz/models.py
+++ Server/golfplatz/models.py
@@ -14,7 +14,7 @@ class Student(AbstractUser):
     phone_validator = RegexValidator(regex=r'^\d{9}$', message="Phone number must be exactly 9 digits long")
 
     student_number = models.CharField(max_length=10, unique=True)
-    phone_number = models.CharField(validators=[phone_validator], max_length=9, unique=True)
+    phone_number = models.CharField(validators=[phone_validator], max_length=9, unique=True, null=True)
 
     def __str__(self):
         return self.get_full_name() + ' (' + self.username + ', ' + self.email + ')'
@@ -66,6 +66,17 @@ class TimerRule(models.Model):
     least_points_awarded_percent = models.PositiveSmallIntegerField()
     rule_end_time = models.PositiveSmallIntegerField()
     decreasing_method = models.CharField(max_length=4, choices=DecreasingMethod.choices, default=DecreasingMethod.NONE)
+
+
+class Path(models.Model):
+    from_adventure = models.ForeignKey('Adventure', on_delete=models.CASCADE)
+    to_adventure = models.ForeignKey('Adventure', on_delete=models.CASCADE)
+    students = models.ManyToManyField('Student', through='PathCoverage')
+
+
+class PathCoverage(models.Model):
+    adventure_started_time = models.DateTimeField()
+    time_elapsed_seconds = models.PositiveSmallIntegerField()
 
 
 class PointSource(models.Model):
