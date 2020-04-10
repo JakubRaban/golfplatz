f7f9b3eb1b08aecac8ea22b63abaa4616bf66c8a
Mateusz Kubicki <drevan657@gmail.com>
2020-04-10 21:52:02 +0200

    Added register student and tutor endpoints

--- Server/golfplatz/views.py
+++ Server/golfplatz/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework import status
 
 from Server.utils import JsonResponse
-from golfplatz.forms import AddCourseForm
+from golfplatz.forms import AddCourseForm, RegisterStudentForm, RegisterTutorForm
 
 
 class AddCourse(APIView):
@@ -19,3 +16,22 @@ class AddCourse(APIView):
         else:
             return JsonResponse({'error': course.errors}, response_code=status.HTTP_409_CONFLICT).get_response()
 
+
+class RegisterStudent(APIView):
+    def post(self, request, format=None):
+        student = RegisterStudentForm(request.data)
+        if student.is_valid():
+            student.save()
+            return JsonResponse({'status': 'student registered'}).get_response()
+        else:
+            return JsonResponse({'error': student.errors}, response_code=status.HTTP_409_CONFLICT).get_response()
+
+
+class RegisterTutor(APIView):
+    def post(self, request, format=None):
+        tutor = RegisterTutorForm(request.data)
+        if tutor.is_valid():
+            tutor.save()
+            return JsonResponse({'status': 'tutor registered'}).get_response()
+        else:
+            return JsonResponse({'error': tutor.errors}, response_code=status.HTTP_409_CONFLICT).get_response()
