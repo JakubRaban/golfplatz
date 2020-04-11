b53307073f0ef9311429f29fae366b2d2e7a131a
Jakub Raban <jakub.raban@gmail.com>
2020-04-11 09:52:39 +0000

    Update urls.py

--- Server/golfplatz/urls.py
+++ Server/golfplatz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import CourseView, RegisterStudent, RegisterTutor
 
 urlpatterns = [
-    path('courses/', AddCourse.as_view()),
+    path('courses/', CourseView.as_view()),
     path('register/tutor', RegisterTutor.as_view()),
     path('register/student', RegisterStudent.as_view()),
 ]
