192847db3c0ed6126f78372dd9a141119788e538
Mateusz Kubicki <drevan657@gmail.com>
2020-05-02 20:38:38 +0200

    Much better approach to adding courses requests

--- Server/frontend/src/actions/course.js
+++ Server/frontend/src/actions/course.js
@@ -7,49 +7,43 @@ import {createMessage, returnErrors} from "./messages";
 export const addCourse = (course, courseGroups, plotParts) => (dispatch, getState) => {
   let courseId;
   let body = {plotParts};
-  axios.post("/api/courses/", course, tokenConfig(getState)).then(res=>{
+  makeAllRequests(course, plotParts, courseGroups, dispatch, getState);
+};
+
+async function makeAllRequests(course, plotParts, courseGroups, dispatch, getState) {
+  let courseId = -1;
+  await axios.post("/api/courses/", course, tokenConfig(getState)).then(res=>{
     dispatch({
       type: ADD_COURSE,
       payload: res.data
     });
     courseId = res.data.id;
-    return axios.post("/api/courses/" + courseId + "/plot_parts/", plotParts[0], tokenConfig(getState))}).then(res=>{
-      dispatch({
-        type: ADD_PLOT_PARTS,
-        payload: res.data
-      })
-    }).catch(err => {
-    dispatch(returnErrors(err.response.data, err.response.status))
+  }).catch(err => {
+    console.log(err);
+    dispatch(returnErrors(err.response.data, err.response.status));
   });
-  console.log(plotParts);
-};
-  // const body = JSON.stringify({ plotParts.get(0) });
-  // console.log(body);
 
-  // axios.post("/api/courses/" + 1 + "/plot_parts/", plotParts[0], tokenConfig(getState)).then(res=>{
-  //   dispatch(createMessage({courseAdded: "Kurs dodany pomyślnie"}));
-  //   dispatch({
-  //     type: ADD_PLOT_PARTS,
-  //     payload: res.data
-  //   });
-  // }).catch(err => {
-  //   console.log(err);
-  //   dispatch(returnErrors(err.response.data, err.response.status));
-  // });
+  await axios.post("/api/courses/" + courseId + "/plot_parts/", plotParts[0], tokenConfig(getState)).then(res=>{
+    dispatch(createMessage({courseAdded: "Kurs dodany pomyślnie"}));
+    dispatch({
+      type: ADD_PLOT_PARTS,
+      payload: res.data
+    });
+  }).catch(err => {
+    dispatch(returnErrors(err.response.data, err.response.status));
+  });
 
-  // axios.post("/api/courses/" + courseId + "/course_groups/", course, tokenConfig(getState)).then(res => {
-  //   dispatch(createMessage({courseAdded: "Kurs dodany pomyślnie"}));
-  //   dispatch({
-  //     type: ADD_COURSE_GROUPS,
-  //     payload: res.data
-  //   });
-  //   courseId = res.data.id;
-  // }).catch(err => {
-  //   dispatch(returnErrors(err.response.data, err.response.status));
-  // });
+  await axios.post("/api/courses/" + courseId + "/course_groups/", courseGroups, tokenConfig(getState)).then(res => {
+    dispatch(createMessage({courseAdded: "Kurs dodany pomyślnie"}));
+    dispatch({
+      type: ADD_COURSE_GROUPS,
+      payload: res.data
+    });
+  }).catch(err => {
+    dispatch(returnErrors(err.response.data, err.response.status));
+  });
+}
 
-  
-};
 
 
 export const getCourses = () => (dispatch, getState) => {
