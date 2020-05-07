6e7c54f3ef7a5cd2e7166e632ee9ee80714563d2
Mateusz Kubicki <drevan657@gmail.com>
2020-05-07 23:45:36 +0200

    New style concept

--- Server/frontend/src/components/tutorComponents/AddCourseInitialInfo.js
+++ Server/frontend/src/components/tutorComponents/AddCourseInitialInfo.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+import Dialog from '@material-ui/core/Dialog';
+import TextField, { Input } from '@material/react-text-field';
+import Button from '@material-ui/core/Button';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: green,
+  },
+});
 
 export class AddCourseInitialInfo extends Component {
   static propTypes = {
@@ -14,32 +28,43 @@ export class AddCourseInitialInfo extends Component {
     this.props.nextStep();
   };
 
+  
+
   render() {
     const { values, handleChange } = this.props;
     return(
-      <div>
-        <div>
-          <label>Podaj nazwę kursu</label>
-          <input
-            type="text"
-            name="name"
-            onChange={handleChange('name')}
-            value={values.name}
-          />
-        </div>
-        <div>
-          <label>Krótko opisz kurs</label>
-          <textarea
-            type="text"
-            name="description"
-            onChange={handleChange('description')}
-            value={values.description}
-          />
-        </div>
-        <button onClick={this.continue}>
-          Dalej
-        </button>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <React.Fragment>
+        <Dialog 
+            open="true"
+            fullWidth="true"
+            maxWidth='sm'
+          >
+          <h2>Wprowadź podstawowe informacje o kursie:</h2>    
+          <TextField label = "Podaj nazwę kursu" margin="normal"W>
+            <Input
+              type="text"
+              name="name"
+              onChange={handleChange('name')}
+              value={values.name}
+              />
+          </TextField>
+          <TextField label = "Krótko opisz kurs" margin="normal"W>
+            <Input
+              type="text"
+              name="description"
+              onChange={handleChange('description')}
+              value={values.description}
+              />
+          </TextField>
+          <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Dalej</Button>
+          </Dialog>
+        </React.Fragment>
+      </MuiThemeProvider>
     )
   }
 }
