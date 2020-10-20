1139100bc6cc6928d8641394e8f02873e0ec9fe0
Mateusz Kubicki <mkubicki@student.agh.edu.pl>
2020-10-20 23:56:03 +0200

    Improve achievement creator style

--- Server/frontend/src/components/tutorComponents/addCourse/Achievement.js
+++ Server/frontend/src/components/tutorComponents/addCourse/Achievement.js
@@ -1,6 +1,7 @@
 import '../../../styles/course-forms.css';
 
-import { MenuItem, Select, Typography } from '@material-ui/core';
+import { Accordion, AccordionDetails, AccordionSummary, MenuItem, Select, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import React, { Component } from 'react';
 
 
@@ -21,77 +22,80 @@ export class AddAchievement extends Component {
   render() {
     const { achievement } = this.props;
     return (
-      <React.Fragment>
-        <Typography variant="subtitle2" gutterBottom>
-          {this.props.index + 1} odznaka
-        </Typography>
-        <div className="row">
-          <div className="col-25">
-            <label className="label-class">Podaj nazwę odznaki:</label>
+      <Accordion key={this.props.index}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+          <Typography variant='subtitle1'>{achievement.name || 'Rozwiń panel'}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className='achievements-wrapper'>
+            <div className="row">
+              <div className="col-25">
+                <label className="label-class">Podaj nazwę odznaki:</label>
+              </div>
+              <div className="col-75">
+                <input className="input-class" value={achievement.name} type="text"
+                  name="name" onChange={() => this.props.handleChange('name', this.props.index, event.target.value)}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label className="label-class">Dodaj obrazek odznaki:</label>
+              </div>
+              <div className="col-75">
+                <input type="file" name="image" accept="image/*" onChange={this.handleImage}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label className="label-class">Podaj jakiego elementu kursu dotyczy odznaka:</label>
+              </div>
+              <div className="col-75">
+                <Select name={'courseElementConsidered'} value={achievement.courseElementConsidered}
+                  onChange={this.handleSelect}>
+                  <MenuItem value={'NOT SELECTED'}>(wybierz)</MenuItem>
+                  <MenuItem value={'PLOT_PART'}>Części fabuły</MenuItem>
+                  <MenuItem value={'CHAPTER'}>Rozdziału</MenuItem>
+                </Select>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-50">
+                <label className="label-class">Podaj liczbę elementów wymaganych do otrzymania odznaki, określ czy muszą wystąpić pod rząd:</label>
+              </div>
+              <div className="col-25">
+                <input className="input-class" value={achievement.howMany} type="number"
+                  name="howMany" onChange={() => this.props.handleChange('howMany', this.props.index, event.target.value)}/>
+              </div>
+              <div className="col-25">
+                <input value={achievement.inARow} type="checkbox"
+                  name="inARow" onChange={() => this.props.handleChange('inARow', this.props.index, event.target.checked)}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label className="label-class">Podaj typ sprawdzanego warunku:</label>
+              </div>
+              <div className="col-75">
+                <Select name={'conditionType'} value={achievement.conditionType}
+                  onChange={this.handleSelect}>
+                  <MenuItem value={'NOT SELECTED'}>(wybierz)</MenuItem>
+                  <MenuItem value={'TIME'}>Czas</MenuItem>
+                  <MenuItem value={'SCORE'}>Liczba punktów</MenuItem>
+                </Select>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-50">
+                <label className="label-class">Podaj minimalny wynik procentowy wymagany do zdobycia tej odznaki:</label>
+              </div>
+              <div className="col-50">
+                <input className="input-class" value={achievement.percentage} type="number"
+                  name="percentage" onChange={() => this.props.handleChange('percentage', this.props.index, event.target.value)}/>
+              </div>
+            </div>
           </div>
-          <div className="col-75">
-            <input className="input-class" value={achievement.name} type="text"
-              name="name" onChange={() => this.props.handleChange('name', this.props.index, event.target.value)}/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label className="label-class">Dodaj obrazek odznaki:</label>
-          </div>
-          <div className="col-75">
-            <input type="file" name="image" accept="image/*" onChange={this.handleImage}/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label className="label-class">Podaj jakiego elementu kursu dotyczy odznaka:</label>
-          </div>
-          <div className="col-75">
-            <Select name={'courseElementConsidered'} value={achievement.courseElementConsidered}
-              onChange={this.handleSelect}>
-              <MenuItem value={'NOT SELECTED'}>(wybierz)</MenuItem>
-              <MenuItem value={'PLOT_PART'}>Części fabuły</MenuItem>
-              <MenuItem value={'CHAPTER'}>Rozdziału</MenuItem>
-            </Select>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-50">
-            <label className="label-class">Podaj liczbę elementów wymaganych do otrzymania odznaki, określ czy muszą wystąpić pod rząd:</label>
-          </div>
-          <div className="col-25">
-            <input className="input-class" value={achievement.howMany} type="number"
-              name="howMany" onChange={() => this.props.handleChange('howMany', this.props.index, event.target.value)}/>
-          </div>
-          <div className="col-25">
-            <input value={achievement.inARow} type="checkbox"
-              name="inARow" onChange={() => this.props.handleChange('inARow', this.props.index, event.target.checked)}/>
-
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label className="label-class">Podaj typ sprawdzanego warunku:</label>
-          </div>
-          <div className="col-75">
-            <Select name={'conditionType'} value={achievement.conditionType}
-              onChange={this.handleSelect}>
-              <MenuItem value={'NOT SELECTED'}>(wybierz)</MenuItem>
-              <MenuItem value={'TIME'}>Czas</MenuItem>
-              <MenuItem value={'SCORE'}>Liczba punktów</MenuItem>
-            </Select>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-50">
-            <label className="label-class">Podaj minimalny wynik procentowy wymagany do zdobycia tej odznaki:</label>
-          </div>
-          <div className="col-50">
-            <input className="input-class" value={achievement.percentage} type="number"
-              name="percentage" onChange={() => this.props.handleChange('percentage', this.props.index, event.target.value)}/>
-          </div>
-        </div>
-      </React.Fragment>
+        </AccordionDetails>
+      </Accordion>
     );
   }
 }
