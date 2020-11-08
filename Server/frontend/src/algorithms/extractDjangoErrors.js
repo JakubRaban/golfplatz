0a1236127333d4209d6bd0f1488f6e15a71ec959
Jakub <jakub.raban@gmail.com>
2020-11-08 22:56:07 +0100

    Remove console.log

--- Server/frontend/src/algorithms/extractDjangoErrors.js
+++ Server/frontend/src/algorithms/extractDjangoErrors.js
@@ -6,16 +6,13 @@ const isString = (value) => typeof value === 'string' || value instanceof String
 const extractDjangoErrors = (djangoErrors, outputErrors = []) => {
   if(isObject(djangoErrors)) {
     for(let value of Object.values(djangoErrors)) {
-      console.log("object", value)
       extractDjangoErrors(value, outputErrors);
     }
   } else if(isArray(djangoErrors)) {
-    console.log("array", djangoErrors)
     for (let value of djangoErrors) {
       extractDjangoErrors(value, outputErrors);
     }
   } else if(isString(djangoErrors)) {
-    console.log("string", djangoErrors)
     outputErrors.push(djangoErrors);
   }
   return outputErrors;
